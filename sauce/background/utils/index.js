8e40eef49e47903babee6218cfe023f03a4e34f1
ctrl07 <skgaming7869@gmail.com>
2026-04-08 01:59:54 -0400

    cleanup: we accidently built a framework

--- sauce/background/utils/index.js
+++
@@ -1,8 +0,0 @@
-/* These utilities are pure logic and must not touch DOM or Chrome APIs. */
-
-export * from './stringUtils.js';
-export * from './storageUtils.js';
-export * from './validationUtils.js';
-export * from './sectionUtils.js';
-export * from './pageTypeUtils.js';
-export * from './cmsBehaviorUtils.js';
